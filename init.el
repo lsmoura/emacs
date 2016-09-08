af3b7bd2a9f166f3bbc3ae9547645f0d5a042ff1
Sergio Moura <sergio@moura.us>
2016-09-08 07:48:52 -0300

    Backup settings

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
 (defconst +emacs-dir+ (concat +home-dir+ "/emacs"))
 (defconst +emacs-lib-dir+ (concat +emacs-dir+ "/plugins"))
 (defconst +emacs-snippets-dir+ (concat +emacs-dir+ "/snippets"))
+(defconst +emacs-backup-dir+ (concat +emacs-dir+ "/backup"))
 
 (custom-set-variables
  '(tool-bar-mode nil)
@@ -13,6 +14,33 @@
 (custom-set-faces
  )
 
+;; Backups
+; Reference: https://www.emacswiki.org/emacs/ForceBackups
+(setq vc-make-backup-files t)
+(setq version-control     t  ;; Use version numbers for backups.
+      kept-new-versions   10 ;; Number of newest versions to keep.
+      kept-old-versions   0  ;; Number of oldest versions to keep.
+      delete-old-versions t  ;; Don't ask to delete excess backup versions.
+      backup-by-copying   t) ;; Copy all files, don't rename them.
+
+; Backup save location
+(setq backup-directory-alist '(("" . (concat +emacs-backup-dir+ "/per-save"))))
+(defun force-backup-of-buffer ()
+  ;; Make a special "per session" backup at the first save of each
+  ;; emacs session.
+  (when (not buffer-backed-up)
+    ;; Override the default parameters for per-session backups.
+    (let ((backup-directory-alist '(("" . (concat +emacs-backup-dir+ "/per-session"))))
+	  (kept-new-versions 3))
+      (backup-buffer)))
+  ;; Make a "per save" backup on each save.  The first save results in
+  ;; both a per-session and a per-save backup, to keep the numbering
+  ;; of per-save backups consistent.
+  (let ((buffer-backed-up nil))
+    (backup-buffer)))
+(add-hook 'before-save-hook  'force-backup-of-buffer)
+
+;; Move lines
 (add-to-list 'load-path +emacs-lib-dir+)
 (require 'move-line)
 (global-set-key (kbd "M-<up>") 'move-line-up)
